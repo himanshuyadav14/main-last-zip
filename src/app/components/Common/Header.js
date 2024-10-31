830abd2d17d59f9a7d8252976b1ae60a7dbf5f20
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-10-31 12:55:01 +0530

    header fixed

--- src/app/components/Common/Header.js
+++ src/app/components/Common/Header.js
@@ -1,41 +1,19 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import HeaderContent from "./HeaderContent";
+import HeaderContent from "./HeaderContent"
 
-const Header = () => {
-  const [headerData, setHeaderData] = useState(null);
+const Header = async () => {
   const url = process.env.NEXT_PUBLIC_SERVER_URL;
-
-  useEffect(() => {
-    const fetchHeaderData = async () => {
-      const options = {
-        method: "GET",
-        cache: "no-store",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-      try {
-        const res = await fetch(`${url}/smile-dental/headers`, options);
-        if (!res.ok) {
-          throw new Error(`Error: ${res.status} ${res.statusText}`);
-        }
-        const data = await res.json();
-        setHeaderData(data);
-      } catch (error) {
-        console.error("Fetch error:", error);
-        setHeaderData({}); // Or handle error state
-      }
-    };
-
-    fetchHeaderData();
-  }, [url]); // Run effect when url changes
+  const options = {
+    method: "GET",
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(`${url}/smile-dental/headers`, options);
+  const headerData = await response.json();
 
   return (
-    <div>
-      {headerData ? <HeaderContent headerData={headerData} /> : <p>Loading...</p>}
-    </div>
+    <HeaderContent headerData={headerData} />
   );
 };
 
